82ec5a56df4b65930ccf353d01ebc4ace28a37b5
CarlosAlfredoOliveiraDeLima <carlos.alfredo.oliveira.de.lima@gmail.com>
2025-07-16 02:11:35 -0300

    Initial commit

---
+++ src/test/java/com/project/portfolio/audiotranscriptionapi/AudioTranscriptionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.portfolio.audiotranscriptionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioTranscriptionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
